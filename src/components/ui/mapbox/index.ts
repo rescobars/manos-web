30b25e666b578e92914be97756f0568227744bec
Robin Escobar <rescobar@buildwithin.com>
2025-10-04 22:51:53 -0600

    clean up map box

--- src/components/ui/mapbox/index.ts
+++ src/components/ui/mapbox/index.ts
@@ -1,5 +1,2 @@
-export { BaseMap, useMap } from './BaseMap';
-export { MapMarkers } from './MapMarkers';
-export { DriverMarkers } from './DriverMarkers';
 export { MapControls } from './MapControls';
 export { DriverMap } from './DriverMap';
